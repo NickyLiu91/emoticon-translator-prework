252cddc680725a40832549563d34a42c0df7469f
Nicky Liu <nickyliu91@gmail.com>
2018-08-24 11:56:09 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,10 +5,16 @@ def load_library(file)
   list = YAML.load_file(file)
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file, emoticon)
   # code goes here
+  list = load_library(file)
+  list.each do |hash, array|
+    if array.include?(emoticon)
+      return array[1]
+    end
+  end
 end
 
-def get_english_meaning
+def get_english_meaning(file, emoticon)
   # code goes here
 end
